5d915bf3fed81f316faaa26d832245ce47bf0e08
Wongsatorn Thongrod <thongrodw@gmail.com>
2024-11-11 00:45:12 +0700

    Implement Pagination

--- src/main/java/com/ata/java_springboot/controllers/SalaryController.java
+++ src/main/java/com/ata/java_springboot/controllers/SalaryController.java
@@ -1,7 +1,9 @@
 package com.ata.java_springboot.controllers;
 
 import com.ata.java_springboot.services.SalaryService;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.RequiredArgsConstructor;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
@@ -19,15 +20,17 @@ public class SalaryController {
     private final SalaryService salaryService;
 
     @GetMapping
-    public List<Object[]> getSalary(
-            @RequestParam(required = false) Map<String, String> filterAttributes,
+    public List<ObjectNode> getSalary(
+            @RequestParam(required = false) MultiValueMap<String, String> filterAttributes,
             @RequestParam(required = false) String fields,
             @RequestParam(required = false) String sort,
-            @RequestParam(required = false, defaultValue = "ASC") String sort_type) {
+            @RequestParam(required = false, defaultValue = "ASC") String sort_type,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size ) {
 
         List<String> fieldList = fields != null ? Arrays.asList(fields.split(",")) : null;
         List<String> sortList = sort != null ? Arrays.asList(sort.split(",")) : null;
 
-        return salaryService.getSalaryByCriteria(filterAttributes, fieldList, sortList, sort_type);
+        return salaryService.getSalaryByCriteria(filterAttributes, fieldList, sortList, sort_type, page, size);
     }
 }
--- src/main/java/com/ata/java_springboot/services/SalaryService.java
+++ src/main/java/com/ata/java_springboot/services/SalaryService.java
@@ -1,15 +1,18 @@
 package com.ata.java_springboot.services;
 
 import com.ata.java_springboot.entities.Salary;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.criteria.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.util.MultiValueMap;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
+import java.util.Objects;
 
 @RequiredArgsConstructor
 @Service
@@ -18,23 +21,38 @@ public class SalaryService {
     @PersistenceContext
     private final EntityManager entityManager;
 
-    public List<Object[]> getSalaryByCriteria(Map<String, String> filters, List<String> selectedFields,
-                                              List<String> sortFields, String sortDirection) {
+    private final ObjectMapper objectMapper;
+
+    public List<ObjectNode> getSalaryByCriteria(MultiValueMap<String, String> filters, List<String> selectedFields,
+                                                List<String> sortFields, String sortDirection, int page, int size) {
 
         CriteriaBuilder cb = entityManager.getCriteriaBuilder();
         CriteriaQuery<Object[]> query = cb.createQuery(Object[].class);
         Root<Salary> root = query.from(Salary.class);
 
         // Select Fields
-        query.multiselect(getSelectedFields(root, selectedFields));
+        List<String> fieldsToSelect = (selectedFields == null || selectedFields.isEmpty())
+                ? getAllFieldNames(root)
+                : selectedFields;
+
+        query.multiselect(getSelectedFields(root, fieldsToSelect));
 
         // Filters
-        query.where(getFilterPredicates(filters, cb, root));
+        if (filters != null && !filters.isEmpty()) {
+            query.where(getFilterPredicates(filters, cb, root));
+        }
 
         // Sorting
-        query.orderBy(getSortOrders(sortFields, sortDirection, cb, root));
+        if (sortFields != null && !sortFields.isEmpty()) {
+            query.orderBy(getSortOrders(sortFields, sortDirection, cb, root));
+        }
 
-        return entityManager.createQuery(query).getResultList();
+        List<Object[]> resultList = entityManager.createQuery(query)
+                .setFirstResult(page * size)
+                .setMaxResults(size)
+                .getResultList();
+
+        return mapObjectsToJson(fieldsToSelect, resultList);
     }
 
     private List<Selection<?>> getSelectedFields(Root<Salary> root, List<String> fields) {
@@ -45,23 +63,23 @@ public class SalaryService {
         return selections;
     }
 
-    private Predicate[] getFilterPredicates(Map<String, String> filters, CriteriaBuilder cb, Root<Salary> root) {
+    private Predicate[] getFilterPredicates(MultiValueMap<String, String> filters, CriteriaBuilder cb, Root<Salary> root) {
         List<Predicate> predicates = new ArrayList<>();
 
         if (filters.containsKey("jobTitle")) {
-            predicates.add(cb.equal(root.get("jobTitle"), filters.get("jobTitle")));
+            predicates.add(cb.equal(root.get("jobTitle"), filters.getFirst("jobTitle")));
         }
 
         if (filters.containsKey("gender")) {
-            predicates.add(cb.equal(root.get("gender"), filters.get("gender")));
+            predicates.add(cb.equal(root.get("gender"), filters.getFirst("gender")));
         }
 
         if (filters.containsKey("salary[gte]")) {
-            predicates.add(cb.greaterThanOrEqualTo(root.get("salary"), Double.valueOf(filters.get("salary[gte]"))));
+            predicates.add(cb.greaterThanOrEqualTo(root.get("salary"), Double.valueOf(Objects.requireNonNull(filters.getFirst("salary[gte]")))));
         }
 
         if (filters.containsKey("salary[lte]")) {
-            predicates.add(cb.lessThanOrEqualTo(root.get("salary"), Double.valueOf(filters.get("salary[lte]"))));
+            predicates.add(cb.lessThanOrEqualTo(root.get("salary"), Double.valueOf(Objects.requireNonNull(filters.getFirst("salary[lte]")))));
         }
 
         return predicates.toArray(new Predicate[0]);
@@ -76,5 +94,25 @@ public class SalaryService {
         }
 
         return orders;
+    }
+
+    private List<ObjectNode> mapObjectsToJson(List<String> fields, List<Object[]> objects) {
+        List<ObjectNode> jsonObjects = new ArrayList<>();
+
+        for (Object[] row : objects) {
+            ObjectNode jsonNode = objectMapper.createObjectNode();
+            for (int i = 0; i < fields.size(); i++) {
+                jsonNode.putPOJO(fields.get(i), row[i]);
+            }
+            jsonObjects.add(jsonNode);
+        }
+
+        return jsonObjects;
+    }
+
+    private List<String> getAllFieldNames(Root<Salary> root) {
+        List<String> allFields = new ArrayList<>();
+        root.getModel().getDeclaredSingularAttributes().forEach(attr -> allFields.add(attr.getName()));
+        return allFields;
     }
 }
